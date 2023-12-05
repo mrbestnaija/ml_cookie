49456dadbd9cd26d1d9f0fb21722014a849f7df6
mrbestnaija <ezekwu77@gmail.com>
2023-12-05 13:42:40 +0100

    Added the cookiecutter template mandatory directory and DVC iniatlisation directory

---
+++ my_dvc_project/cookiecutter/mrbestnaija_project_one/setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Machine Learning Test Project for data version control using DVC running on GitHub Actions.',
+    author='Bestman Ezekwu Enock',
+    license='MIT',
+)
